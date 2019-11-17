708593be9bc7ad733c92d4cd0911920c78fe2f78
Malcolm Childs <michilds0@gmail.com>
2019-11-17 02:15:58 -0500

    Adds code to clean build folder on new build

--- src/components/InstallPrompt.js
+++ src/components/InstallPrompt.js
@@ -12,10 +12,3 @@ const InstallPrompt = ({ children }) => (
 );
 
 export default InstallPrompt;
-// window.addEventListener("beforeinstallprompt", function(e) {
-//     // log the platforms provided as options in an install prompt 
-//     console.log(e.platforms); // e.g., ["web", "android", "windows"] 
-//     e.userChoice.then(function(choiceResult) { 
-//       console.log(choiceResult.outcome); // either "accepted" or "dismissed"
-//     }, handleError); 
-//   });
